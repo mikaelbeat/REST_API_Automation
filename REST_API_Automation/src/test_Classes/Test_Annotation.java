707e61b2720f0886205f3671839e7c59ce403fa6
mikaelbeat <ryynanenphm@gmail.com>
2018-04-06 12:00:51 +0300

    Update

--- REST_API_Automation/src/test_Classes/Test_Annotation.java
+++ REST_API_Automation/src/test_Classes/Test_Annotation.java
@@ -1,13 +1,25 @@
 package test_Classes;
 
 import org.testng.annotations.Test;
+import application_Code.Some_Class_To_Test;
 
 public class Test_Annotation {
-  
-  @Test
-  public void f() {
-  
-  
-  }
-  
+
+	@Test
+	public void testMethod1() {
+		Some_Class_To_Test obj = new Some_Class_To_Test();
+		int result = obj.sumNumbers(1, 2);
+		System.out.println("Running test -> testMethod1");
+	}
+	
+	@Test
+	public void testMethod2() {
+		System.out.println("Running test -> testMethod2");
+	}
+	
+	@Test
+	public void testMethod3() {
+		System.out.println("Running test -> testMethod3");
+	}
+
 }
