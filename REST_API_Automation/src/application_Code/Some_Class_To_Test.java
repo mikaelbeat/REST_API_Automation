ae3bc93c222a83ec892709bac409c2a9a122306a
mikaelbeat <ryynanenphm@gmail.com>
2018-04-06 11:15:29 +0300

    Update

---
+++ REST_API_Automation/src/application_Code/Some_Class_To_Test.java
@@ -0,0 +1,9 @@
+package application_Code;
+
+public class Some_Class_To_Test {
+	
+	public int sumNumbers(int a, int  b) {
+		return a + b;
+	}
+
+}
---
+++ REST_API_Automation/src/test_Classes/Test_Annotation.java
@@ -0,0 +1,13 @@
+package test_Classes;
+
+import org.testng.annotations.Test;
+
+public class Test_Annotation {
+  
+  @Test
+  public void f() {
+  
+  
+  }
+  
+}
