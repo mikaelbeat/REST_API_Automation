5072457eddc4821c79c0506f1cfcb032b00cb19c
mikaelbeat <ryynanenphm@gmail.com>
2018-04-10 16:12:30 +0300

    Update

---
+++ REST_API_Automation/TestNG_Training/test_Classes/TestNG_Grouping.java
@@ -0,0 +1,39 @@
+package test_Classes;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestNG_Grouping {
+	
+  @BeforeClass(alwaysRun = true)
+  public void beforeClass() {
+	  System.out.println("Before class.");
+  }
+  
+  @Test(groups = {"cars", "suv"})
+  public  void testBMW6() {
+	  System.out.println("Running test -> BMW X6");
+  }
+  
+  @Test(groups = {"cars", "sedan"})
+  public  void testAudiA6() {
+	  System.out.println("Running test -> Audi A6");
+  }
+  
+  @Test(groups = {"bikes"})
+  public void testNinja() {
+	  System.out.println("Running test -> Kawasaki Ninja");
+  }
+  
+  @Test(groups = {"bikes"})
+  public void testHondaCBR() {
+	  System.out.println("Running test -> Honda CBR");
+  }
+  
+  @AfterClass(alwaysRun = true)
+  public void afterClass() {
+	  System.out.println("After class.");
+  }
+   
+}
