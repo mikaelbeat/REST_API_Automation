30fe9c28b9ccb5c7adc23d270bc4054f442e8474
mikaelbeat <ryynanenphm@gmail.com>
2018-04-13 12:22:14 +0300

    Update

---
+++ REST_API_Automation/TestNG_Training/listeners_Package/CustomerListener1.java
@@ -0,0 +1,23 @@
+package listeners_Package;
+
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestResult;
+
+public class CustomerListener1 implements IInvokedMethodListener{
+
+	@Override
+	public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+			// Before every method in test class
+			System.out.println("beforeInvocation: " + testResult.getTestClass()
+			.getName() + " => " + method.getTestMethod().getMethodName());
+	}
+
+	@Override
+	public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+			// After every method in test class
+			System.out.println("afterInvocation: " + testResult.getTestClass()
+			.getName() + " => " + method.getTestMethod().getMethodName());
+	}
+
+}
