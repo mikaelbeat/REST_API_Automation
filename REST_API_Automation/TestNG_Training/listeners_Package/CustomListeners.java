59856cef96422e515f440c83ad435470796f7f67
mikaelbeat <ryynanenphm@gmail.com>
2018-04-17 09:18:14 +0300

    Update

---
+++ REST_API_Automation/TestNG_Training/listeners_Package/CustomListeners.java
@@ -0,0 +1,86 @@
+package listeners_Package;
+
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
+import org.testng.ITestResult;
+
+public class CustomListeners implements IInvokedMethodListener, ITestListener, ISuiteListener{
+	
+	@Override
+	public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+			// Before every method in test class
+			System.out.println("beforeInvocation: " + testResult.getTestClass()
+			.getName() + " => " + method.getTestMethod().getMethodName());
+	}
+
+	@Override
+	public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+			// After every method in test class
+			System.out.println("afterInvocation: " + testResult.getTestClass()
+			.getName() + " => " + method.getTestMethod().getMethodName());
+	}
+	
+	@Override
+	public void onTestStart(ITestResult result) {
+		// When @Test method starts
+		System.out.println("onTestStart ->  Test name: " + result.getName());
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// If @Test method is successful
+		System.out.println("onTestSuccess -> Test name: " + result.getName());
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// If @Test method is failed
+		System.out.println("onTestFailure -> Test name: " + result.getName());
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// If the @Test method is skipped
+		System.out.println("SKIPPED -> Test name: " + result.getName());
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// Ignore
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// Runs before <test> tag in xml file
+		System.out.println("onStart -> Test tag name: " + context.getName());
+		ITestNGMethod methods[] = context.getAllTestMethods();
+		System.out.println("These methods will be executed in this test tag: ");
+		for(ITestNGMethod method : methods) {
+			System.out.println(method.getMethodName());
+		}
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// Runs after <test> tag in xml file
+		System.out.println("onFinish -> Test tag name: " + context.getName());
+	}
+	
+	@Override
+	public void onStart(ISuite suite) {
+		// When <suite> tag starts
+		System.out.println("onStart: before suite starts");
+	}
+
+	@Override
+	public void onFinish(ISuite suite) {
+		// When <suite> tag completes
+		System.out.println("onFinish: after suite completes");
+	}
+	
+}
