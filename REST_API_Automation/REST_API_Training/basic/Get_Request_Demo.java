bdb527898d41b88f09e235906f6abe96406c4318
mikaelbeat <ryynanenphm@gmail.com>
2018-04-19 15:58:36 +0300

    My first rest api test in java

--- REST_API_Automation/REST_API_Training/basic/Get_Request_Demo.java
+++ REST_API_Automation/REST_API_Training/basic/Get_Request_Demo.java
@@ -1,5 +1,31 @@
 package basic;
 
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class Get_Request_Demo {
+	
+	// Given -> I have this information
+	// When -> I perform this action
+	// Then -> This should be the output
+	
+	@BeforeClass
+	public  void setUp() {
+		RestAssured.baseURI = "https://maps.googleapis.com";
+		RestAssured.basePath = "/maps/api";
+	}
+
+	@Test
+	public void status_Code_Verification() {
+		given().param("units", "imperial")
+			   .param("origins", "Washington,DC")
+			   .param("destinations", "New+York+City")
+			   .param("key", "AIzaSyD9jDyAwytS-zudoP9PyNh-AyFXzwOTX-Q")
+		.when().get("/distancematrix/json")
+		.then().statusCode(200);
+	}
 
 }
