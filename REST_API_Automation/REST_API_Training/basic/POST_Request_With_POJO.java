c285ce4cbae0286f483cad00fabfe847bbd49780
mikaelbeat <ryynanenphm@gmail.com>
2018-04-20 13:21:34 +0300

    Update

--- REST_API_Automation/REST_API_Training/basic/POST_Request_With_POJO.java
+++ REST_API_Automation/REST_API_Training/basic/POST_Request_With_POJO.java
@@ -3,11 +3,18 @@ package basic;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import models.Places_Add_Model;
 
 public class POST_Request_With_POJO {
 	
@@ -23,23 +30,31 @@ public class POST_Request_With_POJO {
 
 	@Test
 	public void validate_Response() {
+		
+		Map<String, Double> locationMap = new HashMap<String, Double>();
+		locationMap.put("lat", -33.8669710);
+		locationMap.put("lng", 151.1958750);
+		
+		List<String> types = new ArrayList<String>();
+		types.add("shoe_store");
+		
+		Places_Add_Model places = new Places_Add_Model();
+		places.setLocation(locationMap);
+		places.setAccuracy(50);
+		places.setName("Google Shoes!");
+		places.setPhone_number("(02) 9374 4000");
+		places.setAddress("48 Pirrama Road, Pyrmont, NSW 2009, Australia");
+		places.setTypes(types);
+		places.setWebsite("http://www.google.com.au");
+		places.setLanquage("en-AU");
+		
+//		Response res = 
 		given()
 			   .queryParam("key", "AIzaSyBiLi7Twf08yLo1GzR-szoYvguotlIu1eQ")
-			   .body("{\r\n" + 
-			   		"  \"location\": {\r\n" + 
-			   		"    \"lat\": -33.8669710,\r\n" + 
-			   		"    \"lng\": 151.1958750\r\n" + 
-			   		"  },\r\n" + 
-			   		"  \"accuracy\": 50,\r\n" + 
-			   		"  \"name\": \"Google Shoes!\",\r\n" + 
-			   		"  \"phone_number\": \"(02) 9374 4000\",\r\n" + 
-			   		"  \"address\": \"48 Pirrama Road, Pyrmont, NSW 2009, Australia\",\r\n" + 
-			   		"  \"types\": [\"shoe_store\"],\r\n" + 
-			   		"  \"website\": \"http://www.google.com.au/\",\r\n" + 
-			   		"  \"language\": \"en-AU\"\r\n" + 
-			   		"}")
+			   .body(places)
 		.when()
 			   .post("/place/add/json")
+//		System.out.println(res.asString());
 		.then()
 				.statusCode(200).and()
 				.contentType(ContentType.JSON).and()
