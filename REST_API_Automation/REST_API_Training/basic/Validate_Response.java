0cbbe8065c92d18878ce18180570e2393e180324
mikaelbeat <ryynanenphm@gmail.com>
2018-04-20 09:24:36 +0300

    Update

---
+++ REST_API_Automation/REST_API_Training/basic/Validate_Response.java
@@ -0,0 +1,38 @@
+package basic;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static org.hamcrest.Matchers.equalTo;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Validate_Response {
+	
+	// Given -> I have this information
+	// When -> I perform this action
+	// Then -> This should be the output
+	
+	@BeforeClass
+	public  void setUp() {
+		RestAssured.baseURI = "https://maps.googleapis.com";
+		RestAssured.basePath = "/maps/api";
+	}
+
+	@Test
+	public void validate_Response() {
+		given().param("units", "imperial")
+			   .param("origins", "Washington,DC")
+			   .param("destinations", "New+York+City")
+			   .param("key", "AIzaSyA49xhTOHmabT9vU8Li-Pu6qdc8HEtawi8")
+		.when().get("/distancematrix/json")
+		.then().statusCode(200)
+		.and()
+		.body("rows[0].elements[0].distance.text", equalTo("225 mi"))
+		.contentType(ContentType.JSON);
+	}
+
+}
